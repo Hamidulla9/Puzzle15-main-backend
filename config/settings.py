5cb5222904d6d92e57fa04ea47c671d30a66eb74
Hamidulla9 <mahamadjonxoldorov9@gmail.com>
2025-04-24 00:58:28 +0500

    1

--- config/settings.py
+++ config/settings.py
@@ -120,7 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # GitHubga chiqqanda foydali bo‘ladi
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Бу муҳими!
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Агар сиз `static/` ичида файллар сақлаётган бўлсангиз
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
